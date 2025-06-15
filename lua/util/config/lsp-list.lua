28724ffac1dbad48b1ae91a61cb7a24be017004b
Senichenkov <Petua41@yandex.ru>
2025-06-15 20:46:43 +0300

    Add CMake LSP

--- lua/util/config/lsp-list.lua
+++ lua/util/config/lsp-list.lua
@@ -2,7 +2,7 @@
 M = {}
 
 -- A list of servers. Add them here
-M.list = { 'clangd', 'lua_ls', 'pylsp', 'gopls' }
+M.list = { 'clangd', 'lua_ls', 'pylsp', 'gopls', 'cmake' }
 
 -- Provides a lot of useful functions and can be used in generic for directly
 M.iter = vim.iter(M.list)
