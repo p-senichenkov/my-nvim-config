5d355c8b0fe6a388e9e4b53a86b235ba4703c958
Senichenkov <Petua41@yandex.ru>
2025-12-18 17:14:37 +0300

    Add "edit" shortkey for gitrebase

--- after/ftplugin/gitrebase.lua
+++ after/ftplugin/gitrebase.lua
@@ -1,3 +1,4 @@
 -- Some convenient key bindings
 vim.keymap.set('n', '<leader>f', '0cwfixup<Esc>', { buffer = true, desc = 'Fixup current line' })
 vim.keymap.set('n', '<leader>s', '0cwsquash<Esc>', { buffer = true, desc = 'Squash current line' })
+vim.keymap.set('n', '<leader>e', '0cwedit<Esc>', { buffer = true, desc = 'Edit current line' })
