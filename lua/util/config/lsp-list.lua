9b12bcdb0f453b83be49816ad62547b036d25ba5
Senichenkov <Petua41@yandex.ru>
2025-12-18 17:15:29 +0300

    (WIP) Edit Python formatter config

--- lua/util/config/lsp-list.lua
+++ lua/util/config/lsp-list.lua
@@ -2,7 +2,7 @@
 M = {}
 
 -- A list of servers. Add them here
-M.list = { 'clangd', 'lua_ls', 'pylsp', 'gopls', 'cmake', 'java_language_server' }
+M.list = { 'clangd', 'lua_ls', 'pylsp', 'gopls', 'cmake' }
 
 -- Provides a lot of useful functions and can be used in generic for directly
 M.iter = vim.iter(M.list)
@@ -50,7 +50,6 @@ M.configs = {
 					},
 					yapf = {
 						enabled = true,
-						executable = 'yapf'
 					},
 					black = {
 						enabled = false
