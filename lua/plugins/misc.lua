7a51b4cf0bcf00d3398ed30920a13049f31e1969
Petr Senichenkov <psenichenkov@yandex-team.ru>
2026-02-02 22:39:37 +0300

    Use ufo for folds

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,64 +1,76 @@
 --[[ Misc plugins ]] --
 return {
-	-- NeoTree
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		-- It's not really needed to open directory (and hijack netrw)
-		keys = {
-			{ '<leader>-', '<cmd>Neotree toggle reveal_force_cwd<cr>', desc = 'Toggle NeoTree' },
-		},
-	},
+    -- NeoTree
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        -- It's not really needed to open directory (and hijack netrw)
+        keys = {
+            { '<leader>-', '<cmd>Neotree toggle reveal_force_cwd<cr>', desc = 'Toggle NeoTree' },
+        },
+    },
 
-	-- More smart comments
-	{
-		'numToStr/Comment.nvim',
-		config = true,
-		keys = {
-			{ 'gcc' },
-			{ 'gbc' },
-			{ 'gc', mode = { 'n', 'v' } },
-			{ 'gb', mode = { 'n', 'v' } },
-			{ 'gcO' },
-			{ 'gco' },
-			{ 'gcA' }
-		},
-	},
+    -- More smart comments
+    {
+        'numToStr/Comment.nvim',
+        config = true,
+        keys = {
+            { 'gcc' },
+            { 'gbc' },
+            { 'gc', mode = { 'n', 'v' } },
+            { 'gb', mode = { 'n', 'v' } },
+            { 'gcO' },
+            { 'gco' },
+            { 'gcA' }
+        },
+    },
 
-	-- More smart analyzer for highlighting
-	{
-		'nvim-treesitter/nvim-treesitter',
-		-- Cannot use `ft` here, since comments can appear in any file
-		lazy = false,
-		branch = 'master',
-		build = function()
-			require('nvim-treesitter.install').update({ with_sync = true })()
-		end,
-		config = function()
-			require('plugins-config/treesitter')
-		end,
-	},
+    -- More smart analyzer for highlighting
+    {
+        'nvim-treesitter/nvim-treesitter',
+        -- Cannot use `ft` here, since comments can appear in any file
+        lazy = false,
+        branch = 'master',
+        build = function()
+            require('nvim-treesitter.install').update({ with_sync = true })()
+        end,
+        config = function()
+            require('plugins-config/treesitter')
+        end,
+    },
 
-	-- Brackets auto-pairing
-	{
-		'windwp/nvim-autopairs',
-		dependencies = { 'hrsh7th/nvim-cmp' },
-		event = 'InsertEnter',
-		config = true,
-		-- NOTE: This plugin can work with specific Treesitter nodes, if needed
-	},
+    -- Brackets auto-pairing
+    {
+        'windwp/nvim-autopairs',
+        dependencies = { 'hrsh7th/nvim-cmp' },
+        event = 'InsertEnter',
+        config = true,
+        -- NOTE: This plugin can work with specific Treesitter nodes, if needed
+    },
 
-	-- Git conflict higlight and keymaps
-	{
-		'akinsho/git-conflict.nvim',
-		-- Update only when "version" is updated
-		version = '*',
-		opts = {},
-		event = 'User CustomGitConflict',
-	}
+    -- Git conflict higlight and keymaps
+    {
+        'akinsho/git-conflict.nvim',
+        -- Update only when "version" is updated
+        version = '*',
+        opts = {},
+        event = 'User CustomGitConflict',
+    },
+
+    -- "Ultimate FOlds"
+    {
+        'kevinhwang91/nvim-ufo',
+        dependencies = {
+            -- NOTE: nvim-treesitter package is not needed
+            'kevinhwang91/promise-async',
+        },
+        config = function()
+            require('plugins-config.folds')
+        end
+    },
 }
--- lua/util/config/colors.lua
+++ lua/util/config/colors.lua
@@ -1,60 +1,66 @@
 M = {}
 
 M.colors = {
-	border_color = '#13afff',
-	line_nr_dark_gray = '#303030',
-	line_nr_gray = '#606060',
+    border_color = '#13afff',
+    line_nr_dark_gray = '#303030',
+    line_nr_gray = '#606060',
 
-	-- Treesitter
-	neon_orange_bg = '#401f10',
-	dark_cyan = '#004f5e',
-	neon_red_bg = '#5e0f00',
-	neon_yellow_bg = '#005e10',
-	rifle_green = '#414833',
+    -- Treesitter
+    neon_orange_bg = '#401f10',
+    dark_cyan = '#004f5e',
+    neon_red_bg = '#5e0f00',
+    neon_yellow_bg = '#005e10',
+    rifle_green = '#414833',
 
-	-- It's not exactly Cryola blue
-	cryola_blue = '#1f55dc',
-	-- everybody knows that ghosts are dark-violet
-	ghost_violet = '#5d1b64',
-	frost_sky = '#00a6fa',
+    -- It's not exactly Cryola blue
+    cryola_blue = '#1f55dc',
+    -- everybody knows that ghosts are dark-violet
+    ghost_violet = '#5d1b64',
+    frost_sky = '#00a6fa',
+    -- It's very different from built-in DarkBlue
+    dark_blue = '#002137',
 }
 
 M.highlights = {
-	-- Line numbers and related
-	['LineNr'] = { fg = '$line_nr_dark_gray' },
-	['CursorLineNr'] = { fg = '$line_nr_gray' },
-	['ColorColumn'] = { bg = 'magenta' },
+    -- Line numbers and related
+    ['LineNr'] = { fg = '$line_nr_dark_gray' },
+    ['CursorLineNr'] = { fg = '$line_nr_gray' },
+    ['ColorColumn'] = { bg = 'magenta' },
 
-	-- Treesitter
-	['@comment.todo'] = { fg = 'orange', bg = '$neon_orange_bg', bold = true },
-	['@comment.note'] = { fg = 'cyan', bg = '$dark_cyan', bold = true },
-	['@comment.error'] = { fg = 'red', bg = '$neon_red_bg', bold = true },
-	['@comment.warning'] = { fg = 'yellow', bg = '$neon_yellow_bg', bold = true },
-	['@comment.documentation'] = { italic = true, fg = '$rifle_green' },
+    -- Treesitter
+    ['@comment.todo'] = { fg = 'orange', bg = '$neon_orange_bg', bold = true },
+    ['@comment.note'] = { fg = 'cyan', bg = '$dark_cyan', bold = true },
+    ['@comment.error'] = { fg = 'red', bg = '$neon_red_bg', bold = true },
+    ['@comment.warning'] = { fg = 'yellow', bg = '$neon_yellow_bg', bold = true },
+    ['@comment.documentation'] = { italic = true, fg = '$rifle_green' },
 
-	-- nvim-cmp
-	['cmp-float-border'] = { fg = '$border_color' },
-	['CmpItemAbbrMatch'] = { fg = '$cryola_blue', bg = 'NONE' },
-	-- ['CmpItemAbbrMatchFuzzy'] = { link = 'CmpItemAbbrMatch' },
-	['GhostText'] = { fg = '$ghost_violet' },
+    -- nvim-cmp
+    ['cmp-float-border'] = { fg = '$border_color' },
+    ['CmpItemAbbrMatch'] = { fg = '$cryola_blue', bg = 'NONE' },
+    -- ['CmpItemAbbrMatchFuzzy'] = { link = 'CmpItemAbbrMatch' },
+    ['GhostText'] = { fg = '$ghost_violet' },
 
-	-- LSP diagnostics
-	['DiagnosticError'] = { fg = 'red' },
-	['DiagnosticWarn'] = { fg = 'magenta' },
-	['DiagnosticHint'] = { fg = 'yellow' },
-	['DiagnosticInfo'] = { fg = '$frost_sky' },
-	['FloatBorder'] = { bg = 'bg', fg = '$border_color' },
-	['NormalFloat'] = { bg = 'Normal' },
+    -- LSP diagnostics
+    ['DiagnosticError'] = { fg = 'red' },
+    ['DiagnosticWarn'] = { fg = 'magenta' },
+    ['DiagnosticHint'] = { fg = 'yellow' },
+    ['DiagnosticInfo'] = { fg = '$frost_sky' },
+    ['FloatBorder'] = { bg = 'bg', fg = '$border_color' },
+    ['NormalFloat'] = { bg = 'Normal' },
 
-	-- I don't want concealed items to be specially highlighted
-	['Conceal'] = { bg = 'Normal', fg = 'Normal' },
+    -- I don't want concealed items to be specially highlighted
+    ['Conceal'] = { bg = 'Normal', fg = 'Normal' },
 
-	-- LaTeX `\comment` command. NOTE this name is matched against custom command automatically
-	['texCCommentArg'] = { fg = 'magenta', fmt = 'italic' },
+    -- LaTeX `\comment` command. NOTE this name is matched against custom command automatically
+    ['texCCommentArg'] = { fg = 'magenta', fmt = 'italic' },
+
+    -- Folds
+    ['Folded'] = { bg = M.colors.dark_blue },
+    ['FoldColumn'] = { bg = 'Normal', fg = '$line_nr_dark_gray' },
 }
 
 M.misc = {
-	['IblScope'] = { fg = M.colors.dark_cyan, nocombine = true },
+    ['IblScope'] = { fg = M.colors.dark_cyan, nocombine = true },
 }
 
 return M
