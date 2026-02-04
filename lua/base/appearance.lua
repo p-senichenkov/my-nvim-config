b220aa2541737f83e255082060d3911b1be60363
Petr Senichenkov <psenichenkov@yandex-team.ru>
2026-02-04 15:59:53 +0300

    Highlight too long lines with dark magenta

--- lua/base/appearance.lua
+++ lua/base/appearance.lua
@@ -16,5 +16,8 @@ vim.opt.cursorline = true
 -- Max completion popup size
 vim.opt.pumheight = 20
 
+-- Highlight too long lines (compared to colorcolumn, highlights only bad lines)
+vim.fn.matchadd('ColorColumn', '\\%101v', 100)
+
 -- Enable 'conceal' feature (visually replace one text with another)
 vim.opt.conceallevel = 2
