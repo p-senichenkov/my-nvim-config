c9591a538fc249dfcb4ded435d22c57381123f5b
Senichenkov <Petua41@yandex.ru>
2026-04-03 22:59:29 +0300

    Use continuous border

--- lua/util/config/float.lua
+++ lua/util/config/float.lua
@@ -2,7 +2,7 @@
 M = {}
 
 -- Dashed rounded border
-M.border = { '╭', '╌', '╮', '┊', '╯', '╌', '╰', '┊'}
+M.border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│'}
 
 -- Only border is visible. Colors are specified in 'base/colors'
 M.winhighlight = 'Normal:Normal,NormalFloat:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None'
